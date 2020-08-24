cf23471cba9d45945da824b4223367fd04a8c0d1
Powerkyria <gitteando@gmail.com>
2020-08-24 16:25:09 +0100

    add js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-console.log('>> Ready :)');
+console.log('Hola');
+
+const user = document.querySelectorAll('.flex');
+const html = document.getElementsByTagName('html');
+
+
+function animation() {
+    let scrollTop = html.scrollTop;
+    console.log(scrollTop);
+	for (let i = 0; i < user.length; i++) {
+		let heightUser = user[i].offsetTop;
+
+		if (heightUser < scrollTop) {
+			user[i].style.opacity = 1;
+		}
+		if (heightUser -300 < scrollTop) {
+			user[i].classList.add('flex:nth-of-type(2)');
+		}
+		if (heightUser -300 < scrollTop) {
+			user[i].classList.add('flex:nth-of-type(3)');
+		}
+		if (heightUser -300 < scrollTop) {
+			user[i].classList.add('flex:nth-of-type(4)');
+		}
+	}
+}
+window.addEventListener('scroll', animation);
